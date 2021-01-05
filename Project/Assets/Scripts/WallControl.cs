7d0b98054659c959e441d796ebdf2abf216cf2b8
iperet <71767484+iperet@users.noreply.github.com>
2021-01-06 00:06:19 +0200

    Add files via upload

---
+++ Project/Assets/Scripts/WallControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallControl : MonoBehaviour
+
+{
+    public static bool isMoving = true;
+    private GameObject player;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") isMoving=false; 
+    }
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
